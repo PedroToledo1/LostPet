e5ef4ef9fb65d01a40ed0524e5f4641d6b962d83
PedroToledo1 <59672144+PedroToledo1@users.noreply.github.com>
2023-08-14 16:40:48 -0300

    Initial Commit

---
+++ LostPet/LostPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  LostPetApp.swift
+//  LostPet
+//
+//  Created by Pedro Toledo on 14/8/23.
+//
+
+import SwiftUI
+
+@main
+struct LostPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
