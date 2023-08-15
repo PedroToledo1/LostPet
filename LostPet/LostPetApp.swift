a824dd528ebdb394e4dfd688d819adaa4ca3abd7
PedroToledo1 <59672144+PedroToledo1@users.noreply.github.com>
2023-08-14 22:59:23 -0300

    Update LostPetApp.swift

--- LostPet/LostPetApp.swift
+++ LostPet/LostPetApp.swift
@@ -22,6 +22,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
+        print("firebase configurado")
         return true
     }
 }
