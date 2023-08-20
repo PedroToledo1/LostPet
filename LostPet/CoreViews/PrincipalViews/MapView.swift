2854c6d8825640162e8796132c9df75d200c19c4
PedroToledo1 <59672144+PedroToledo1@users.noreply.github.com>
2023-08-20 01:58:33 -0300

    location and user location

--- LostPet/CoreViews/PrincipalViews/MapView.swift
+++ LostPet/CoreViews/PrincipalViews/MapView.swift
@@ -10,11 +10,16 @@ import MapKit
 
 struct MapView: View {
     @StateObject private var userLocation = LocationViewModel()
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516, longitude: -121.891054), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+    
     
     var body: some View {
-        Map(coordinateRegion: $region, showsUserLocation: true)
+        Map(coordinateRegion: $userLocation.region , showsUserLocation: true)
             .ignoresSafeArea()
+            .accentColor(Color(.systemGreen))
+            .onAppear{
+                userLocation.checkIfLocationServicesIsEnable()
+            }
+        
     }
 }
 
--- LostPet/CoreViews/PrincipalViews/UserLocationManager.swift
+++ LostPet/CoreViews/PrincipalViews/UserLocationManager.swift
@@ -8,19 +8,22 @@
 import Foundation
 import MapKit
 
-final class LocationViewModel: ObservableObject{
+final class LocationViewModel: NSObject, ObservableObject, CLLocationManagerDelegate{
+    @Published var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516, longitude: -121.891054), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+    
     var locationManager: CLLocationManager?
     
     func checkIfLocationServicesIsEnable(){
         if CLLocationManager.locationServicesEnabled() {
             locationManager = CLLocationManager()
-            locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+            checkLocationAuthorization()
+            locationManager!.delegate = self
         }else{
             print("La ubicaion gato, dale activala")
         }
     }
     
-    func checkLocationAuthorization(){
+    private func checkLocationAuthorization(){
         guard let locationManager = locationManager else {return}
         switch locationManager.authorizationStatus{
             
@@ -31,9 +34,14 @@ final class LocationViewModel: ObservableObject{
         case .denied:
             print("This app will need location permission to upload the markers. if not you will only be able too see the ones that already exist")
         case .authorizedAlways, .authorizedWhenInUse:
-            break
+            region = MKCoordinateRegion(center: locationManager.location!.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
         @unknown default:
             break
         }
     }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAuthorization()
+    }
+    
 }
