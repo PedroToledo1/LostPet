6f388600cc3ad62415ec19f254ea9b1984def90e
PedroToledo1 <59672144+PedroToledo1@users.noreply.github.com>
2023-08-20 16:05:22 -0300

    fixes

--- LostPet/CoreViews/PrincipalViews/NavigationBar.swift
+++ LostPet/CoreViews/PrincipalViews/NavigationBar.swift
@@ -36,7 +36,7 @@ struct NavigationBar: View {
                 }
             }
             .onAppear{
-                //locationManager.checkIfLocationServiceIsEnable()
+                
             }
         }
     }
