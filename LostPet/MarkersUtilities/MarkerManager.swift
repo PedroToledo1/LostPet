7375bc4e903f6b0608183dc8dbd82fc86d119a04
PedroToledo1 <59672144+PedroToledo1@users.noreply.github.com>
2023-08-29 15:30:17 -0300

    Update MarkerManager.swift

--- LostPet/MarkersUtilities/MarkerManager.swift
+++ LostPet/MarkersUtilities/MarkerManager.swift
@@ -17,36 +17,73 @@ import GeoFireUtils
 import CoreLocationUI
 
 
-struct marcadoresFinal: Encodable, Decodable, Decoder{
-    
+
+struct MarkerArray: Codable {
+    let marcadores: [Markers]
+}
+
+struct Markers: Identifiable, Codable, Equatable {
+    var id = UUID()
     let markerId: String
-    let date: Date!
-    let photourl: String!
-    let coordinates: GeoPoint?
-    
+    let date: String?
+    let photourl: String?
+    let coordinates: Int?
     
     enum CodingKeys: String, CodingKey {
-        case markerId = "markerid"
-        case photourl = "photomarker"
-        case date = "date"
-        case coordinates = "coordinates"
+        case markerId
+        case date
+        case photourl
+        case coordinates
+    }
+}
+final class MarkerManager {
+    
+    static let shared = MarkerManager()
+    private init() { }
+    
+    private let markerCollection = Firestore.firestore().collection("markers")
+    
+    private func markerDocument(markerID: String) -> DocumentReference {
+        markerCollection.document(markerID)
     }
     
-    init(from decoder: Decoder) throws {
-            let container = try decoder.container(keyedBy: CodingKeys.self)
-            self.markerId = try container.decode(String.self, forKey: .markerId)
-            self.photourl = try container.decodeIfPresent(String.self, forKey: .photourl)!
-            self.date = try container.decodeIfPresent(Date.self, forKey: .date)!
-            self.coordinates = try container.decodeIfPresent(GeoPoint.self, forKey: .coordinates)
-        }
-    init(from decoder: Decoder) throws {
-            let container = try decoder.container(keyedBy: CodingKeys.self)
-            self.markerId = try container.decode(String.self, forKey: .markerId)
-            self.photourl = try container.decodeIfPresent(String.self, forKey: .photourl)!
-            self.date = try container.decodeIfPresent(Date.self, forKey: .date)!
-            self.coordinates = try container.decodeIfPresent(GeoPoint.self, forKey: .coordinates)
-        }
+    func markerProduct(marker: Markers) async throws {
+        try markerDocument(markerID: marker.markerId).setData(from: marker, merge: false)
+    }
+    
+    func getProduct(markerID: String) async throws -> Markers {
+        try await markerDocument(markerID: markerID).getDocument(as: Markers.self)
+    }
 }
+//struct marcadoresFinal: Encodable, Decodable, Decoder{
+//    let markerId: String
+//    let date: Date!
+//    let photourl: String!
+//    let coordinates: GeoPoint?
+//
+//
+//    enum CodingKeys: String, CodingKey {
+//        case markerId = "markerid"
+//        case photourl = "photomarker"
+//        case date = "date"
+//        case coordinates = "coordinates"
+//    }
+//
+//    init(from decoder: Decoder) throws {
+//            let container = try decoder.container(keyedBy: CodingKeys.self)
+//            self.markerId = try container.decode(String.self, forKey: .markerId)
+//            self.photourl = try container.decodeIfPresent(String.self, forKey: .photourl)!
+//            self.date = try container.decodeIfPresent(Date.self, forKey: .date)!
+//            self.coordinates = try container.decodeIfPresent(GeoPoint.self, forKey: .coordinates)
+//        }
+//    init(from decoder: Decoder) throws {
+//            let container = try decoder.container(keyedBy: CodingKeys.self)
+//            self.markerId = try container.decode(String.self, forKey: .markerId)
+//            self.photourl = try container.decodeIfPresent(String.self, forKey: .photourl)!
+//            self.date = try container.decodeIfPresent(Date.self, forKey: .date)!
+//            self.coordinates = try container.decodeIfPresent(GeoPoint.self, forKey: .coordinates)
+//        }
+//}
 //struct MarkerManagerData: Codable {
 //    let markerID: String
 //    let date: Date
@@ -74,110 +111,110 @@ struct marcadoresFinal: Encodable, Decodable, Decoder{
 //
 //}
 
-final class StorageManager: NSObject, ObservableObject, Identifiable, CLLocationManagerDelegate {
-    
-    
-    @Published var loc = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516, longitude: -121.891054), span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005))
-    
-    
-    let markerCollection = Firestore.firestore().collection("markers")
-    
-    static let shared = StorageManager()
-    override init(){
-        super.init()
-        locationManager.delegate = self
-        
-    }
-    private let storage = Storage.storage().reference()
-    //: MARK: Funciones para el guardado de imagenes y obtencion del path
-    
-    private var imagesReference: StorageReference {
-        storage.child("markers")
-    }
-    
-    func saveImage(data: Data) async throws -> (path: String, name: String){
-        let meta = StorageMetadata()
-        meta.contentType = "image/png"
-        print("save image paso 1")
-        let path = "\(UUID().uuidString).png"
-        print(path)
-        let returnMetaData = try await imagesReference.child(path).putDataAsync(data, metadata: meta)
-        print("save image parte 2")
-        
-        guard let ReturnPath = returnMetaData.path, let ReturnName = returnMetaData.name else {
-            throw URLError(.cancelled)
-            
-        }
-        print(ReturnPath)
-        print(ReturnName)
-        return(ReturnPath, ReturnName)
-    }
-    
-    func saveMarkerImage(item: PhotosPickerItem) {
-        Task {
-            guard let data = try await item.loadTransferable(type: Data.self) else { return }
-            print("savemarker image parte 1")
-            let (path, name) = try await StorageManager.shared.saveImage(data: data)
-            print("success")
-            print(path)
-            print(name)
-            requestAllowOnceLocationPermission()
-            let marcadorsito = marcadoresFinal( markerId: UUID().uuidString, date: Date(), photourl: path, coordinates: GeoPoint(latitude: (loc.center.latitude), longitude: (loc.center.longitude)))
-            let uno = try marcadoresFinal(from: marcadorsito)
-            try await newMarker(marcador: uno)
-            print(uno)
-            
-        }
-    }
-    
-    
-    //: MARK: agregar marcadores
-    
-    private func markerDocument(markerID: String) -> DocumentReference {
-        markerCollection.document(markerID)
-    }
-    func newMarker(marcador: marcadoresFinal)async throws{
-        try markerCollection.document(marcador.markerId).setData(from: marcador, merge: false)
-    }
-    
-    //: MARK: marcador especifico
-    func getmarkerImage(marcador: String) async throws -> marcadoresFinal {
-        try await markerDocument(markerID: marcador).getDocument(as: Decodable.self)
-    }
-    
-    
-    
-    //: MARK: geolocalizacion
-    
-    let locationManager = CLLocationManager()
-    
-    func requestAllowOnceLocationPermission(){
-        locationManager.requestLocation()
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let latestLocation = locations.first else {
-            return
-        }
-        DispatchQueue.main.async {
-            self.loc = MKCoordinateRegion(center: latestLocation.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
-        }
-    }
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print(error.localizedDescription)
-    }
-    
-    //:MARK: download marcadores
-    
-    func getAllMarkers() async  throws -> [marcadoresFinal]{
-        let snapshot = try await markerCollection.getDocuments()
-        
-        var markers: [marcadoresFinal] = []
-        
-        for document in snapshot.documents{
-            let marker = try document.data(as: marcadoresFinal.self)
-            markers.append(marker)
-        }
-        return markers
-    }
-}
+//final class StorageManager: NSObject, ObservableObject, Identifiable, CLLocationManagerDelegate {
+//
+//
+//    @Published var loc = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516, longitude: -121.891054), span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005))
+//
+//
+//    let markerCollection = Firestore.firestore().collection("markers")
+//
+//    static let shared = StorageManager()
+//    override init(){
+//        super.init()
+//        locationManager.delegate = self
+//
+//    }
+//    private let storage = Storage.storage().reference()
+//    //: MARK: Funciones para el guardado de imagenes y obtencion del path
+//
+//    private var imagesReference: StorageReference {
+//        storage.child("markers")
+//    }
+//
+//    func saveImage(data: Data) async throws -> (path: String, name: String){
+//        let meta = StorageMetadata()
+//        meta.contentType = "image/png"
+//        print("save image paso 1")
+//        let path = "\(UUID().uuidString).png"
+//        print(path)
+//        let returnMetaData = try await imagesReference.child(path).putDataAsync(data, metadata: meta)
+//        print("save image parte 2")
+//
+//        guard let ReturnPath = returnMetaData.path, let ReturnName = returnMetaData.name else {
+//            throw URLError(.cancelled)
+//
+//        }
+//        print(ReturnPath)
+//        print(ReturnName)
+//        return(ReturnPath, ReturnName)
+//    }
+//
+//    func saveMarkerImage(item: PhotosPickerItem) {
+//        Task {
+//            guard let data = try await item.loadTransferable(type: Data.self) else { return }
+//            print("savemarker image parte 1")
+//            let (path, name) = try await StorageManager.shared.saveImage(data: data)
+//            print("success")
+//            print(path)
+//            print(name)
+//            requestAllowOnceLocationPermission()
+//            let marcadorsito = marcadoresFinal(markerId: UUID().uuidString, date: Date(), photourl: path, coordinates: GeoPoint(latitude: (loc.center.latitude), longitude: (loc.center.longitude)))
+//            let uno = try marcadoresFinal(from: marcadorsito)
+//            try await newMarker(marcador: uno)
+//            print(uno)
+//
+//        }
+//    }
+//
+//
+//    //: MARK: agregar marcadores
+//
+//    private func markerDocument(markerID: String) -> DocumentReference {
+//        markerCollection.document(markerID)
+//    }
+//    func newMarker(marcador: marcadoresFinal)async throws{
+//        try markerCollection.document(marcador.markerId).setData(from: marcador, merge: false)
+//    }
+//
+//    //: MARK: marcador especifico
+//    func getmarkerImage(marcador: String) async throws -> marcadoresFinal {
+//        try await markerDocument(markerID: marcador).getDocument(as: Decodable.self)
+//    }
+//
+//
+//
+//    //: MARK: geolocalizacion
+//
+//    let locationManager = CLLocationManager()
+//
+//    func requestAllowOnceLocationPermission(){
+//        locationManager.requestLocation()
+//    }
+//
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        guard let latestLocation = locations.first else {
+//            return
+//        }
+//        DispatchQueue.main.async {
+//            self.loc = MKCoordinateRegion(center: latestLocation.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+//        }
+//    }
+//    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+//        print(error.localizedDescription)
+//    }
+//
+//    //:MARK: download marcadores
+//
+//    func getAllMarkers() async  throws -> [marcadoresFinal]{
+//        let snapshot = try await markerCollection.getDocuments()
+//
+//        var markers: [marcadoresFinal] = []
+//
+//        for document in snapshot.documents{
+//            let marker = try document.data(as: marcadoresFinal.self)
+//            markers.append(marker)
+//        }
+//        return markers
+//    }
+//}
