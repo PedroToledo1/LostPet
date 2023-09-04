31e923794397e961d76c002776383fa2dc40fa46
PedroToledo1 <59672144+PedroToledo1@users.noreply.github.com>
2023-09-03 23:46:09 -0300

    Update markersmapview.swift

--- LostPet/MapManager/Views/markersmapview.swift
+++ LostPet/MapManager/Views/markersmapview.swift
@@ -26,15 +26,15 @@ struct markersmapview: View {
                     .frame(width: 28)
                         
                 // Image(uiImage: image)
-
-                    AsyncImage(url: markers.photourl)
-//                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 26)
-//                            .shadow(color: .black, radius: 2)
-                            .clipShape(Circle())
+                
+                    AsyncImage(url: url)
+                    //                            .resizable()
+                        .scaledToFit()
+                        .frame(width: 26)
+                    //                            .shadow(color: .black, radius: 2)
+                        .clipShape(Circle())
                     
-                        
+                    
                 
             }
         })
@@ -43,6 +43,6 @@ struct markersmapview: View {
 
 struct markersmapview_Previews: PreviewProvider {
     static var previews: some View {
-        markersmapview(markers: Markers(markerID: "prueba", id: 12, date: Date(), photourl: URL(string: "hola"), coordinates: GeoPoint(latitude: 22.22, longitude: 22.22), imagepath: "kdjnsvnjds"))
+        markersmapview(markers: Markers(markerID: "prueba", id: 12, date: Date(), photourl: URL(string: "https://firebasestorage.googleapis.com:443/v0/b/lostpet-fd80f.appspot.com/o/markers%2F72F567FD-3099-41AC-A05C-6647B015F002.png?alt=media&token=fac76a92-201e-46fd-8fdb-6031eee066cd"), coordinates: GeoPoint(latitude: 22.22, longitude: 22.22), imagepath: "kdjnsvnjds"))
     }
 }
